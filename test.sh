1202b4da38474213d34871d0204e8f5462aed064
vladixuk <vgenkov@hotmail.co.uk>
2018-10-08 20:39:26 +0100

    Initial commit

---
+++ test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+seconds=5
+days=10
+
+while [ $days != 0 ]
+do
+echo "Days:"$days
+seconds=5
+     while [ "$seconds" != "0" ]
+     do
+        echo "seconds:"$seconds
+       (( seconds-- ))
+                sar -P ALL  1 1 |  grep -v Average | grep -A 1 -B 1  %system | grep -v "%system" | awk '{print $1 "," $3 "," $5}' | sed -n '1!p'
+        done
+(( days--))
+done
